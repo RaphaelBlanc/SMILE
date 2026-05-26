b430cf6fc77b791e75d0b4575c3a85974b1dd04f
guilhem <guilhem.fourrey@epita.fr>
2026-05-26 20:20:45 +0200

    Update Boss Glacius mechanics, anti-camp teleport, and cleanup asset paths

--- test_sprite2.py
+++
@@ -1,5 +0,0 @@
-import pygame
-pygame.display.init()
-pygame.display.set_mode((1,1))
-img = pygame.image.load('./assets/images/monstre/boss_glace.png')
-print(f"Size: {img.get_width()}x{img.get_height()}")
